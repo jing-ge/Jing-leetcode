fcc7d4afc369ac68e385815fa6dec0d32a164311
jing-ge <675324370@qq.com>
2020-12-31 21:13:05 +0800

    finish inertview 16.13,16.14 14 still have some code problem

--- interview/16.13.cpp
+++ interview/16.13.cpp
@@ -19,19 +19,82 @@
 
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
+
+void print(vector<vector<double>>&r){
+    for(auto res:r){
+        for(double i:res){
+            cout<<i<<",";
+        }
+        cout<<endl;
+    }
+}
 
 class Solution {
 public:
     vector<double> cutSquares(vector<int>& square1, vector<int>& square2) {
+        double cx1 = square1[0] + square1[2]/2.0;
+        double cy1 = square1[1] + square1[2]/2.0;
+        double cx2 = square2[0] + square2[2]/2.0;
+        double cy2 = square2[1] + square2[2]/2.0;
+        vector<vector<double>>res;
+        cout<<cx1<<","<<cy1<<";"<<cx2<<","<<cy2<<endl;
+        cout<<"-----------"<<endl;
+        if(cx1==cx2){
+            res.push_back({cx1,(double)square1[1]});
+            res.push_back({cx1,(double)square1[1]+square1[2]});
+            res.push_back({cx2,(double)square2[1]});
+            res.push_back({cx1,(double)square2[1]+square2[2]});
+        } else if(cy1 == cy2){
+            res.push_back({(double)square1[0],cy1});
+            res.push_back({(double)square1[0]+square1[2],cy1});
+            res.push_back({(double)square2[0],cy2});
+            res.push_back({(double)square2[0]+square2[2],cy2});
+        }else
+        {
+            double x1 = square1[0] ;
+            double x2 = square1[0]+square1[2];
+            double x3 = square2[0];
+            double x4 = square2[0]+square2[2];
 
+            double y1 = (x1-cx1)/(cx2-cx1)*(cy2-cy1)+cy1;
+            double y2 = (x2-cx1)/(cx2-cx1)*(cy2-cy1)+cy1;;
+            double y3 = (x3-cx1)/(cx2-cx1)*(cy2-cy1)+cy1;;
+            double y4 = (x4-cx1)/(cx2-cx1)*(cy2-cy1)+cy1;;
+
+            double yy1 = square1[1] ;
+            double yy2 = square1[1]+square1[2];
+            double yy3 = square2[1];
+            double yy4 = square2[1]+square2[2];
+
+            double xx1 = (yy1-cy1)/(cy2-cy1)*(cx2-cx1)+cx1;
+            double xx2 = (yy2-cy1)/(cy2-cy1)*(cx2-cx1)+cx1;
+            double xx3 = (yy3-cy1)/(cy2-cy1)*(cx2-cx1)+cx1;
+            double xx4 = (yy4-cy1)/(cy2-cy1)*(cx2-cx1)+cx1;
+            if(x1>=square1[0]&&x1<=square1[0]+square1[2]&&y1>=square1[1]&&y1<=square1[1]+square1[2])res.push_back({x1,y1});
+            if(x2>=square1[0]&&x2<=square1[0]+square1[2]&&y2>=square1[1]&&y2<=square1[1]+square1[2])res.push_back({x2,y2});
+            if(x3>=square2[0]&&x3<=square2[0]+square2[2]&&y3>=square2[1]&&y3<=square2[1]+square2[2])res.push_back({x3,y3});
+            if(x4>=square2[0]&&x4<=square2[0]+square2[2]&&y4>=square2[1]&&y4<=square2[1]+square2[2])res.push_back({x4,y4});
+
+            if(xx1>=square1[0]&&xx1<=square1[0]+square1[2]&&yy1>=square1[1]&&yy1<=square1[1]+square1[2])res.push_back({xx1,yy1});
+            if(xx2>=square1[0]&&xx2<=square1[0]+square1[2]&&yy2>=square1[1]&&yy2<=square1[1]+square1[2])res.push_back({xx2,yy2});
+            if(xx3>=square2[0]&&xx3<=square2[0]+square2[2]&&yy3>=square2[1]&&yy3<=square2[1]+square2[2])res.push_back({xx3,yy3});
+            if(xx4>=square2[0]&&xx4<=square2[0]+square2[2]&&yy4>=square2[1]&&yy4<=square2[1]+square2[2])res.push_back({xx4,yy4});
+        }
+        sort(res.begin(),res.end());
+        return {res[0][0],res[0][1],res.back()[0],res.back()[1]};
     }
 };
 
 int main(){
     Solution s =Solution();
-    vector<int> square1 = {-1, -1, 2},square2 = {0, -1, 2};
-    vector<int> res =s.cutSquares(square1,square2);
+    vector<int> square1 = {68,130,64},square2 = {-230,194,7};
+    vector<double> res =s.cutSquares(square1,square2);
+    for(double i:res){
+        cout<<i<<",";
+    }
+    cout<<endl;
     return 0;
 }
--- interview/16.14.cpp
+++ interview/16.14.cpp
@@ -1,23 +1,26 @@
 // 面试题 16.14. Best Line LCCI
 // Given a two-dimensional graph with points on it, find a line which passes the most number of points.
 
-// Assume all the points that passed by the line are stored in list S sorted by their number. You need to return [S[0], S[1]], that is , two points that have smallest number. If there are more than one line that passes the most number of points, choose the one that has the smallest S[0]. If there are more that one line that has the same S[0], choose the one that has smallest S[1].
+// Assume all the points that passed by the line are stored in list S sorted by their number. You need to return {S{0}, S{1}}, that is , two points that have smallest number. If there are more than one line that passes the most number of points, choose the one that has the smallest S{0}. If there are more that one line that has the same S{0}, choose the one that has smallest S{1}.
 
 // Example:
 
-// Input:  [[0,0],[1,1],[1,0],[2,0]]
-// Output:  [0,2]
-// Explanation:  The numbers of points passed by the line are [0,2,3].
+// Input:  {{0,0},{1,1},{1,0},{2,0}}
+// Output:  {0,2}
+// Explanation:  The numbers of points passed by the line are {0,2,3}.
 // Note:
 
 // 2 <= len(Points) <= 300
-// len(Points[i]) = 2
+// len(Points{i}) = 2
 // 通过次数2,041提交次数3,936
+
 
 #include <iostream>
 #include <vector>
 #include <unordered_map>
 #include <set>
+#include <cmath>
+#include <string>
 
 using namespace std;
 
@@ -31,13 +34,65 @@ void print(vector<int>&res){
 class Solution {
 public:
     vector<int> bestLine(vector<vector<int>>& points) {
+        unordered_map<string,set<int>>map;
+        for(int i=0;i<points.size();i++){
+            for(int j=i+1;j<points.size();j++){
+                string t = linek(points[i],points[j]);
+                cout<<t<<endl;
+                map[t].insert(i);
+                map[t].insert(j);
+            }
+        }
+        int t = 0;
+        vector<int>res;
+        for(auto item:map){
+            if(item.second.size()>t){
+                t = item.second.size();
+                res = vector<int>(item.second.begin(),item.second.end());
+            }
+            if(item.second.size()==t){
+                vector<int> temp(item.second.begin(),item.second.end());
+                if(res[0]>temp[0])res =temp;
+            }
+        }
+        return res; 
+    }
+    string linek(vector<int>&x,vector<int>&y){
+        int A = y[1]-x[1];
+        int B = x[0]-y[0];
+        int C = (long)y[0]*x[1]-(long)x[0]*y[1];
+        int yue = divisor(divisor(abs(A),abs(B)),abs(C));
+        A = A/yue;
+        B = B/yue;
+        C = C/yue;
+        string res = to_string(A) +to_string(B)+ to_string(C);
+        return res;
+    }
+    //辗转相除法求最大公约数函数
+    int divisor(int a, int b) {
+        int temp;
 
+        //比较两个数的大小，值大的数为a，值小的数为b
+        if (a < b) {
+            temp = a;
+            a = b;
+            b = temp;
+        }
+
+        //求余
+        while (b != 0) {
+            temp = a % b;
+            a = b;
+            b = temp;
+        }
+        return a;
     }
 };
 
+
 int main(){
     Solution s = Solution();
-    vector<int>points = {{0,0},{1,1},{1,0},{2,0}};
+    vector<vector<int>>points = {{-38935,27124},{-39837,19604},{-7086,42194},{-11571,-23257},{115,-23257},{20229,5976},{24653,-18488},{11017,21043},{-9353,16550},{-47076,15237}};
     vector<int> res = s.bestLine(points);
     print(res);
     return 0;
